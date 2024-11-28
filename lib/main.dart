4b53b0d17926117e37abf9087ce59373a404c226
shin-na-ra <nas221@naver.com>
2024-11-28 13:41:17 +0900

    로그인페이지로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
-import 'view/home.dart';
+import 'view/login/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
