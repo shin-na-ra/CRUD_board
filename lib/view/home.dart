bbbe5051f1ffdc449c73e91a403e6219eff805c8
shin-na-ra <nas221@naver.com>
2024-11-28 12:46:10 +0900

    로그인페이지 생성

---
+++ lib/view/home.dart
@@ -0,0 +1,24 @@
+import 'package:crud_board/vm/vm_getx.dart';
+import 'package:flutter/material.dart';
+import 'package:get/instance_manager.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final VMgetX controller = Get.put(VMgetX());
+
+    return Scaffold(
+      appBar: controller.myAppBar("로그인"),
+      body: const Center(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
