27f44529ead61a0519627459d771c926245ddb04
shin-na-ra <nas221@naver.com>
2024-11-28 15:25:16 +0900

    유저 모델에 전화번호 추가

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,12 +1,14 @@
 class User {
     String id;
     String name;
+    String telno;
     String password;
     int status;
 
     User({
         required this.id,
         required this.name,
+        required this.telno,
         required this.password,
         required this.status
     });
@@ -14,6 +16,7 @@ class User {
     User.fromMap(Map<String, dynamic> res) 
         : id = res['id'],
         name = res['name'],
+        telno = res['telno'],
         password = res['password'],
         status = res['status'];
 }
