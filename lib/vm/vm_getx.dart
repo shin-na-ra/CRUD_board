70422739536109cf631ffd557b9158bee2d5e571
shin-na-ra <nas221@naver.com>
2024-11-28 13:41:50 +0900

    버튼 함수 생성

--- lib/vm/vm_getx.dart
+++ lib/vm/vm_getx.dart
@@ -1,4 +1,5 @@
 
+import 'package:crud_board/view/login/register.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,11 +7,36 @@ class VMgetX extends GetxController {
 
   AppBar myAppBar(String title) {
     return AppBar(
-        title: const Text(
-          "게시판",
-        ),
+        title:  Text(title),
         foregroundColor: Colors.white,
         backgroundColor: Colors.black,
+      );
+  }
+
+  SizedBox myEleBtn(Color bgColor, Color fontColor, String title, int value) {
+    return SizedBox(
+        width: 200,
+        height: 50,
+        child: ElevatedButton(
+          onPressed: (){
+            if (value == 1 ){
+              print("로그인 클릭");
+            } else {
+              print("회원가입 클릭");
+              Get.to(const RegisterPage());
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: bgColor
+          ),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: fontColor,
+              fontSize: 18
+            ),
+          ),
+        ),
       );
   }
 
