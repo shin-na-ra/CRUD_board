eda1685201b60892a6ebd5a681a78af31fe60f48
shin-na-ra <nas221@naver.com>
2024-11-29 15:34:23 +0900

    게시판 페이지 생성

---
+++ lib/view/board/home.dart
@@ -0,0 +1,94 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_board/model/board.dart';
+import 'package:crud_board/vm/vm_getx.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+import 'addPage.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final VMgetX controller = Get.put(VMgetX());
+    
+
+    return Scaffold(
+      appBar: controller.myAppBar("게시판"),
+      body: Center(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+            .collection('board')
+            .where('status', isEqualTo: 0)
+            // .orderBy('id', descending: false)
+            .snapshots(),
+
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final documents = snapshot.data!.docs;
+            
+            return ListView(
+              children: documents.map((e) => _buildItemWidget(e)).toList(),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconButton(
+            onPressed: () => Get.to(const AddPage()),
+            style: IconButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary
+            ),
+            iconSize: 40,
+            icon: const Icon(
+              Icons.add,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildItemWidget(doc) {
+  final board = Board(
+    id: doc['id'],
+    title: doc['title'],
+    content: doc['content'],
+    date: doc['date'],
+    status: doc['status']
+  );
+
+  return Dismissible(
+    direction: DismissDirection.endToStart,
+    background: Container(
+      color: Colors.red,
+      alignment: Alignment.centerRight,
+      child: const Icon(Icons.delete_forever),
+    ),
+    key: ValueKey(doc),
+    onDismissed: (direction) {
+      FirebaseFirestore.instance
+      .collection('board')
+      .doc(doc.id)
+      .update({'status' : 1});
+    },
+    child: Card(
+      child: ListTile(
+        // title: Text("${board.title} \n\n 작성일 : ${board.date}"),
+        title: Text(board.title),
+        trailing: Text(
+          DateFormat('yyyy-MM-dd').format(board.date.toDate())
+        ),
+      ),
+    ),
+  );
+}
