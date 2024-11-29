3feef5b3756860553c7893b8c4e6109c36b6ebfb
shin-na-ra <nas221@naver.com>
2024-11-29 12:38:50 +0900

    8자까지로 늘림

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -59,7 +59,7 @@ class _LoginPageState extends State<LoginPage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Text('${idController.text.length}/6'),
+                  Text('${idController.text.length}/8'),
                 ],
               ),
             ),
