8af81e770d5a56dc8ab1ceebe03493701b3d12b2
shin-na-ra <nas221@naver.com>
2024-11-29 17:16:34 +0900

    글쓰기 페이지 생성

---
+++ lib/view/board/addPage.dart
@@ -0,0 +1,22 @@
+import 'package:crud_board/view/board/boardFormWidget.dart';
+import 'package:crud_board/vm/vm_getx.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddPage extends StatefulWidget {
+  const AddPage({super.key});
+
+  @override
+  State<AddPage> createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  @override
+  Widget build(BuildContext context) {
+    final VMgetX controller = Get.put(VMgetX());
+    return Scaffold(
+      appBar: controller.myAppBar("게시판 글쓰기"),
+      body: Boardformwidget(),
+    );
+  }
+}
